bc62a0605aa0069a6b5b9ffc59603bc087b6b51f
Andrew <andrej.gorokhoff2017@yandex.ru>
2021-07-02 00:55:39 +0300

    add slider

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 800,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_sl.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right_sl.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+  });
