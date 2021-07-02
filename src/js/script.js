46ca5ea623a7a91e723270573caab1dbf77668b5
Andrew <andrej.gorokhoff2017@yandex.ru>
2021-07-03 02:57:28 +0300

    adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -10,7 +10,16 @@ $(document).ready(function(){
                     dots: true,
                     arrows: false
                 }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                }
             }
+            
         ]
     });
-  });
+  }); 
+
